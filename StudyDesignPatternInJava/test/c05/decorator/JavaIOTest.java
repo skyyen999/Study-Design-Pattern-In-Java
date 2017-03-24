690ddbc856006ab75f3e5eda4def7fca7fe274d7
wYan <skyyen999@gmail.com>
2017-03-24 23:06:03 +0800

    rewrite fileIo

--- StudyDesignPatternInJava/test/c05/decorator/JavaIOTest.java
+++ StudyDesignPatternInJava/test/c05/decorator/JavaIOTest.java
@@ -1,37 +1,42 @@
 package c05.decorator;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.junit.Test;
 
-import c05.decorator.javaIO.UpperCaseInputStream;
+import c05.decorator.javaIO.ReverseReader;
 /**
  * 裝飾模式實例javaIO-測試
  */
 public class JavaIOTest {
+	@SuppressWarnings("resource")
 	@Test
 	public void test() throws IOException{
-
-			// 測試將讀入的字母換成大寫
-			InputStream in = new UpperCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+			System.out.println("=========FileReader讀取檔案==========");
+			FileReader reader = new FileReader("test.txt");
+			int c = reader.read();
+			while (c >= 0) {
+	            System.out.print((char)c);
+	            c = reader.read();
+	        }	
 			
-			int c;
-			// 這是 BufferedInputStream 提供的mark
-			in.mark(0);
-			while(( c = in.read()) >= 0){
-				System.out.print((char)c);
-			}
+			System.out.println("=========BufferedReader讀取檔案==========");
+			BufferedReader bufferedReader = new BufferedReader(new FileReader("test.txt"));
+			String line = bufferedReader.readLine();;
+			while (line!=null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }	
 			
-			// 這是 BufferedInputStream 提供的reset
-			in.reset();
-			System.out.println("--------------reset--------------");
-			while( (c = in.read()) >= 0){
-				System.out.print((char)c);
-			}
-			in.close();
-			
+			System.out.println("=========Reverse Reader反轉讀入的內容==========");
+			// 測試將讀入的句子倒轉
+			ReverseReader reverseReader = new ReverseReader(new FileReader("test.txt"));
+			String rLine = reverseReader.reverseLine();
+			while (rLine!=null) {
+                System.out.println(rLine);
+                rLine = reverseReader.reverseLine();
+            }	
 	}
 }
